28d50629d2796fd73dc434cd95fe33c67796e854
chace <chacenb@gmail.com>
2023-06-03 10:41:21 +0100

    ...

---
+++ wildflydeploy/src/main/java/com/chace/wildflydeploy/WildflydeployApplication.java
@@ -0,0 +1,16 @@
+package com.chace.wildflydeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WildflydeployApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildflydeployApplication.class, args);
+    }
+
+
+    /* link : https://www.mastertheboss.com/jboss-frameworks/spring/spring-boot-hello-world-on-wildfly/ */
+}
